e1671da6546870174b61303eda195e2c2cd82a7f
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2025-08-30 15:13:46 +0300

    add env var for the api url

--- src/assets/constans.ts
+++ src/assets/constans.ts
@@ -1,4 +1,10 @@
- const API_URL = "http://localhost:4000/tasks";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/tasks";
 const TASKS_PER_PAGE = 5;
+const columns = [
+  { key: "backlog", title: "Backlog" },
+  { key: "in-progress", title: "In Progress" },
+  { key: "review", title: "Review" },
+  { key: "done", title: "Done" },
+];
 
- export { API_URL, TASKS_PER_PAGE };
+export { API_URL, TASKS_PER_PAGE, columns };
